6f34352fbac1997181f33c85ba6a30a7f2823ee8
Jamie Cole <jamiejcole@icloud.com>
2023-05-25 02:41:51 +1000

    added basic structure

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown, faMagnifyingGlass, faPhone } from '@fortawesome/free-solid-svg-icons'
+import './App.css';
+
+function App() {
+	return (
+		<div className='w-screen h-screen'>
+			{/* Header Div */}
+			<div className='w-full h-8 bg-darkbg grid grid-cols-3 grid-flow-col gap-24 text-white font-light py-1'>
+				<div className='flex items-center justify-left pl-16 gap-2'>
+					<FontAwesomeIcon icon={faPhone} size="xs"/>
+					<p>+(203) 313 412</p>
+				</div>
+				<div className='flex items-center justify-center'>
+					<p>Get 50% off on Selected Items &ensp;&ensp; | &ensp;&ensp; Shop Now</p>
+				</div>
+				<div className='flex items-center justify-center gap-4'>
+					<div className='flex items-center justify-center gap-2'>
+						<p>Eng</p>
+						<FontAwesomeIcon icon={faChevronDown} size="xs" />
+					</div>
+					<div className='flex items-center justify-center gap-2'>
+						<p>Location</p>
+						<FontAwesomeIcon icon={faChevronDown} size="xs" />
+					</div>
+				</div>
+			</div>
+
+
+			{/* Main Page */}
+			<div className='w-full'>
+				<div className='h-24 flex items-center justify-start gap-8 pl-12'>
+					<div className='font-bold text-lg pr-8'>Shopcart</div>
+					<div className='flex flex-row gap-8 items-center justify-center'>
+						<div className='flex items-center justify-center gap-2'>
+							<p>Categories</p>
+							<FontAwesomeIcon icon={faChevronDown} size="xs" />
+						</div>
+						<div>Deals</div>
+						<div>What's New</div>
+						<div>Delivery</div>
+					</div>
+					<div className='flex items-center justify-start bg-gray-200 gap-4 px-4 py-2 rounded-3xl w-1/5'>
+						<span className='text-gray-500'>Search Product</span>
+						<FontAwesomeIcon className='text-gray-700' icon={faMagnifyingGlass} size="md" />
+					</div>
+				</div>
+			</div>
+
+		</div>
+  	);
+}
+
+export default App;
